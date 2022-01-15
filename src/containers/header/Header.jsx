e91e8e29f925ed81ffd21ea38d7a89fb15fa43cf
Harun Doğdu <harundogdu06@gmail.com>
2022-01-15 18:58:40 +0300

    work on the header components

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Header.scss";
+import mainAIImage from "assets/ai.png";
+import peopleImage from "assets/people.png";
+/* funct */
 const Header = () => {
-    return (
-        <div>
-            Header
+  return (
+    <div className="header">
+      <div className="header__content">
+        <div className="header__content__title">
+          <h1>Let’s Build Something amazing with GPT-3 OpenAI</h1>
         </div>
-    )
-}
+        <div className="header__content__description">
+          <p>
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
+        </div>
+        <div className="header__content__sendEmailBox">
+          <input type="email" placeholder="Enter Your Email Address.." />
+          <button>Get Started</button>
+        </div>
+        <div className="header__content__testimonials">
+          <img src={peopleImage} alt="peopleImage" />
+          <span>1,600 people requested access a visit in last 24 hours</span>
+        </div>
+      </div>
+      <div className="header__mainImage">
+        <img src={mainAIImage} alt="mainImage" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
