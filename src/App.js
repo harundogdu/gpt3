5ec0b42c47f733ff223e901db7e229a5e688e877
Harun Doğdu <harundogdu06@gmail.com>
2022-01-15 18:21:36 +0300

    work on the navbar component

--- src/App.js
+++ src/App.js
@@ -15,17 +15,51 @@ import {
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
-      <Componies />
-      <WGPT3 />
-      <Feature />
-      <Intro />
-      <CTA />
-      <Blog />
-      <Footer />
+      <div className="container">
+        <Navbar />
+        <Header />
+        <Componies />
+        <WGPT3 />
+        <Feature />
+        <Intro />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/* css bem model
+    Block Elements Modifier
+
+    <div className="product-card">
+        <div className="product-card__header">
+            <div className="product-card__header__title">
+            Header
+            <img src="" className="product-card__header__image" />
+            </div>
+        </div>
+        <div className="product-card__body">
+            <div className="product-card__body__title">
+            Body
+            </div>
+        </div>
+    </div>
+
+    <div className="product__card">
+        <div className="product__card-header">
+            <div className="product__card-header__title">
+            Header
+            <img src="" className="product__card-header__image" />
+            </div>
+        </div>
+        <div className="product__card-body">
+            <div className="product__card-body__title">
+            Body
+            </div>
+        </div>
+    </div>
+*/
--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.scss";
+import Logo from "../../assets/logo.svg";
+/* func */
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+  const menuItem = [
+    {
+      id: 1,
+      name: "Home",
+      href: "/",
+    },
+    {
+      id: 2,
+      name: "What is GPT?",
+      href: "#what-is-gpt",
+    },
+    {
+      id: 3,
+      name: "Open AI",
+      href: "#open-ai",
+    },
+    {
+      id: 4,
+      name: "Case Studies",
+      href: "#case-studies",
+    },
+    {
+      id: 5,
+      name: "Library",
+      href: "#library",
+    },
+  ];
+  return (
+    <div className="navbar">
+      <div className="navbar__navbar-logo">
+        <img src={Logo} alt="Logo_Image" />
+      </div>
+      <div className="navbar__navbar-menu">
+        <ul>
+          {menuItem.map((item) => (
+            <li key={item.id}>
+              <a href={item.href}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar__navbar-btn_container">
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
