3861c1168fbc6b8d550275757cb1f09e0ecaf964
Yiğit Uzun <yigit.yiu@gmail.com>
2022-01-16 00:40:56 +0300

    work on the intro components

--- src/containers/intro/Intro.jsx
+++ src/containers/intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./Intro.scss";
+import PossibilityImage from "assets/possibility.png";
 const Intro = () => {
-    return (
-        <div>
-            Intro
-        </div>
-    )
-}
+  return (
+    <div className="intro">
+      <div className="intro__left">
+        <img src={PossibilityImage} alt="possibility" />
+      </div>
+      <div className="intro__right">
+        <h4>Request Early Access to Get Started</h4>
+        <h2>The possibilities are beyond your imagination</h2>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <span>Request Early Access to Get Started</span>
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
